7eab1cd0b1295ff226f5c3eccfaa10a7e3bc5e26
Hack Reactor Students <github@hackreactor.com>
2013-08-30 12:15:58 -0700

    copy chat server files into db repo dir

---
+++ chat_server/src/basic-server.js
@@ -0,0 +1,35 @@
+/* Import node's http module: */
+var mysql = require("mysql");
+var http = require("http");
+var handler = require('./request-handler.js');
+var url = require('url');
+
+/* This is the callback function that will be called each time a
+ * client (i.e.. a web browser) makes a request to our server. */
+
+var port = 8080;
+var ip = "127.0.0.1";
+var server = http.createServer(handler.requestRouter);
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
+
+var dbConnection = mysql.createConnection({
+  user: "blakelie",
+  password: "plantlife",
+  database: "chat"
+});
+
+dbConnection.connect();
+
+dbConnection.end();
+
+
+
+
+
+
+
+
+
+
+
